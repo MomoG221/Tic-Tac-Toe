09f34ffdccf4dfb2b23daac9c0a4ba768fd61709
Mohamed Gueye <mohamed.gueye88@gmail.com>
2024-07-02 20:01:01 -0400

    change name and game board.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-
+import Player from './components/Player.jsx' ;
+import GameBoard from './components/GameBoard.jsx';
 function App() {
   
 
@@ -8,16 +9,13 @@ function App() {
       <div id="game-container">  
 
       <ol id="players">
-        <li>
-        <span className="player-name">Player 1</span>
-        <span className="player-symbol">X</span>
-        </li>
-        <li>
-        <span className="player-symbol">Player 2</span>
-        <span className="player-symbol">O</span>
-        </li>
+
+       <Player initialName="Player 1" symbol="X" />
+       <Player initialName="Player 2" symbol="O" />
 
       </ol>
+      
+      <GameBoard />
       
       </div>
     </main>
