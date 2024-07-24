1cc692e105c135139c1532514157a075ac4b35a6
Mohamed Gueye <mohamed.gueye88@gmail.com>
2024-07-24 00:49:07 +0000

    X and O appears on the grid

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,44 +1,34 @@
 import { useState } from 'react';
 
-export default function Player({initialName, symbol, }) {
+export default function Player({ initialName, symbol, isActive }) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
 
-    const [playerName, setPlayerName] = useState(initialName);
-    const [isEditing, setIsEditing] = useState(false);
+  function handleEditClick() {
+    setIsEditing((editing) => !editing);
+  }
 
-    function handleEditClick() {
-        
-        setIsEditing(!isEditing);
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
 
-    }
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
+  // let btnCaption = 'Edit';
 
-    function handleChange(event){
-        setPlayerName(event.target.value);
+  if (isEditing) {
+    editablePlayerName = (
+      <input type="text" required value={playerName} onChange={handleChange} />
+    );
+    // btnCaption = 'Save';
+  }
 
-    }
-
-    let editablePlayerName = <span className="player-name">{playerName}</span> ;
-    let btnCaption = "Edit"; 
-
-    if (isEditing){
-
-        editablePlayerName = <input type="text" required value={playerName} onChange={handleChange} />
-        btnCaption= 'Save'
-
-    }
-
-    return (
-
-        <li>
-            <span className="player">
-
-                {editablePlayerName}
-                <span className="player-symbol">{symbol}</span>
-
-            </span>
-
-            <button onClick={handleEditClick}>{btnCaption}</button>
-
-        </li>
-    )
-
+  return (
+    <li className={isActive ? 'active' : undefined}>
+      <span className="player">
+        {editablePlayerName}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
+    </li>
+  );
 }
