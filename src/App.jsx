d7e15ee5688acf2f80f8a4c297d6a30e9f5e465f
Mohamed Gueye <mohamed.gueye88@gmail.com>
2024-07-24 02:16:41 +0000

    fixed bugs

--- src/App.jsx
+++ src/App.jsx
@@ -52,7 +52,28 @@ function App() {
 
   }
 
+  let winner = "";
+  for (const combination of WINNING_COMBINATIONS) {
+  
+    const firstSquareSymbol=gameBoard[combination[0].row][combination[0].column];
+    const secondSquareSymbol=gameBoard[combination[1].row][combination[1].column];
+    const thirdSquareSymbol=gameBoard[combination[2].row][combination[2].column];
 
+    
+    if(
+
+      firstSquareSymbol && 
+      firstSquareSymbol === secondSquareSymbol && 
+      firstSquareSymbol === thirdSquareSymbol 
+
+    )
+
+      {
+
+      winner = players[firstSquareSymbol];
+
+  }
+}
 
 const hasDraw = gameTurns.length === 9 && !winner;
 
