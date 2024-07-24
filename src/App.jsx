2451fab946a438c36456b0b47fdedf794cfbe96b
Mohamed Gueye <mohamed.gueye88@gmail.com>
2024-07-24 02:15:02 +0000

    game is finished

--- src/App.jsx
+++ src/App.jsx
@@ -28,6 +28,14 @@ function deriveActivePlayer(gameTurns) {
 }
 
 function App() {
+
+  const [players, setPlayers] = useState({
+
+    'X': 'Player 1',
+    'O': 'Player 2',
+    
+  });
+
   const [gameTurns, setGameTurns] = useState([]);
   
   const activePlayer = deriveActivePlayer(gameTurns);
@@ -44,28 +52,7 @@ function App() {
 
   }
 
-  let winner = "";
-  for (const combination of WINNING_COMBINATIONS) {
-  
-    const firstSquareSymbol=gameBoard[combination[0].row][combination[0].column];
-    const secondSquareSymbol=gameBoard[combination[1].row][combination[1].column];
-    const thirdSquareSymbol=gameBoard[combination[2].row][combination[2].column];
 
-    
-    if(
-
-      firstSquareSymbol && 
-      firstSquareSymbol === secondSquareSymbol && 
-      firstSquareSymbol === thirdSquareSymbol 
-
-    )
-
-      {
-
-      winner = firstSquareSymbol;
-
-  }
-}
 
 const hasDraw = gameTurns.length === 9 && !winner;
 
@@ -87,6 +74,17 @@ const hasDraw = gameTurns.length === 9 && !winner;
     setGameTurns([]);
   }
 
+  function handlePlayerNameChange(symbol, newName) {
+   setPlayers((prevPlayers) => {
+    return {
+      ...prevPlayers,
+      [symbol] : newName
+    };
+    });
+
+  }
+  
+
   return (
     <main>
       <div id="game-container">
@@ -95,11 +93,13 @@ const hasDraw = gameTurns.length === 9 && !winner;
             initialName="Player 1"
             symbol="X"
             isActive={activePlayer === 'X'}
+            onChangeName={handlePlayerNameChange}
           />
           <Player
             initialName="Player 2"
             symbol="O"
             isActive={activePlayer === 'O'}
+            onChangeName={handlePlayerNameChange}
           />
         </ol>
         {(winner || hasDraw) && (<GameOver winner={ winner } onRestart={handleRestart}/>)}
